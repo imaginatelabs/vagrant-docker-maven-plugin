b8b68c6da84aafdd9277e763e4ff9f317f8d1a73
Wayne Allan <wayne.allan@imaginatelabs.com>
2014-04-08 23:40:34 +1000

    fix: The console class now reads the input correctly so that it doesn't hang like it previously was

--- src/main/java/com/imageinatelabs/Console.java
+++ src/main/java/com/imageinatelabs/Console.java
@@ -2,27 +2,26 @@ package com.imageinatelabs;
 
 
 import org.apache.maven.plugin.logging.Log;
+import org.codehaus.plexus.util.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Console {
     public static int exec(String command, String[] envp, File dir, Log log) throws IOException, InterruptedException {
-        //TODO Manage dynamic input e.g. Loading that updates the line
         Runtime rt = Runtime.getRuntime();
         Process pr = rt.exec(command,envp, dir);
-        BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-        BufferedReader error = new BufferedReader(new InputStreamReader(pr.getErrorStream()));
-        String line=null;
-        while((line=input.readLine()) != null) {
+        Scanner scanner = new Scanner(pr.getInputStream());
+        while(scanner.hasNext()){
+            String line = scanner.nextLine();
+            if(StringUtils.isEmpty(line)){
+                line = scanner.next();
+            }
             log.info(line);
         }
-        while((line=error.readLine()) != null) {
-            log.error(line);
-        }
-
         int exitVal = pr.waitFor();
         if(exitVal!=0){
             log.error("Failed with Exit Code " + exitVal);
